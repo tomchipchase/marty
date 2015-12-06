8016e66b18279f56f1b91dbb1c99d7d83b049e60
Tom Chipchase <tom@tomchipchase.co.uk>
2015-12-06 00:59:22 +0000

    added some more methods to hash

--- source/marty/hash.d
+++ source/marty/hash.d
@@ -2,23 +2,89 @@ module marty.hash;
 
 import std.exception : assumeUnique;
 
+/**
+  * A Hash object that is immutable with time travelling features.
+  */
 immutable class Hash(K, V) {
   public:
     alias _data this;
 
+    /**
+     * Initialize a hash with data from a standard hash
+     */
     this(immutable(V[K]) data) pure {
-      _data = data;
-      _previousVersion = null;
+        _data = data;
+        _previousVersion = null;
     }
 
+    ///
+    unittest {
+      immutable data = ["foo": 1];
+      immutable subject = new immutable(Hash!(string, int))(data);
+    }
+
+    /**
+     * Create a new hash with the passed in value set.
+     * Returns: New updated hash object with value inserted.
+     */
     auto insert(in K key, in V value) pure {
-      immutable(V[K]) newHash = [key: value];
-      return new immutable(Hash!(K, V))(newHash, this);
+        immutable(V[K]) newHash = [key: value];
+        return new immutable(Hash!(K, V))(newHash, this);
     }
 
-    V opIndex(in K key) pure {
-      auto ptr = key in _data;
-      return ptr ? *ptr : _previousVersion[key];
+    ///
+    unittest {
+        immutable data = ["foo": 1];
+        immutable subject = new immutable(Hash!(string, int))(data);
+        auto result = subject.insert("bar", 2);
+    }
+
+    /**
+     * Fetch a value from the hash. Raise an error if the key does not exist
+     * in the hash.
+     */
+    V opIndex(in K key) pure @nogc {
+        auto ptr = key in _data;
+        return ptr ? *ptr : _previousVersion[key];
+    }
+
+    ///
+    unittest {
+        immutable data = ["foo": 1];
+        immutable subject = new immutable(Hash!(string, int))(data);
+        auto result = subject.insert("bar", 2);
+        assert(result["bar"] == 2);
+        assert(result["foo"] == 1);
+    }
+
+    /**
+     * Return the previous state of the hash.
+     */
+    auto rollBack() @nogc pure {
+        return _previousVersion;
+    }
+
+    ///
+    unittest {
+        immutable data = ["foo": 1];
+        immutable subject = new immutable(Hash!(string, int))(data);
+        auto result = subject.insert("foo", 2).rollBack;
+        assert(result["foo"] == 1);
+    }
+
+    /**
+     * Returns an updated hash with a key value removed from the hash.
+     */
+    auto remove(K key) pure {
+        return this;
+    }
+
+    ///
+    unittest {
+        immutable data = ["foo": 1];
+        immutable subject = new immutable(Hash!(string, int))(data);
+        auto result = subject.remove("foo");
+        assert(result["foo"] != 1);
     }
 
   private:
@@ -26,27 +92,11 @@ immutable class Hash(K, V) {
     Hash!(K, V) _previousVersion;
 
     this(immutable(V[K]) data, immutable(Hash!(K, V)) previousVersion) pure {
-      _data = data;
-      _previousVersion = cast(immutable)previousVersion;
+        _data = data;
+        _previousVersion = cast(immutable)previousVersion;
     }
 }
 
 unittest {
-  struct Foo {
-    int bar;
-  }
-
-  alias Hsh = immutable(Hash!(string, Foo));
-
-  auto foo = Foo(1);
-  immutable rawHash = ["foo": foo];
-
-  auto subject = new Hsh(rawHash);
-
-  assert(subject["foo"] == foo);
-
-  immutable newSubject = subject.insert("bar", foo);
-  assert(newSubject != subject);
-  assert(newSubject["bar"] == foo);
-  assert(newSubject["foo"] == foo);
+    alias Foo = immutable(Hash!(string, int));
 }
